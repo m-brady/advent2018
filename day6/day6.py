43f881bd8c76e0951fe9b4b4e685229d422b26a0
Michael <mbrady@eyereturn.com>
2018-12-08 22:23:30 -0500

    wip

--- day6/day6.py
+++ day6/day6.py
@@ -1,7 +1,37 @@
 coords = []
-
+max_x = 0
+max_y = 0
 with open('input.txt', 'r') as f:
     for line in f:
-        coords.append(tuple(int(l.strip()) for l in line.split(',')))
+        coord = tuple(int(l.strip()) for l in line.split(','))
+        if coord[0] > max_x:
+            max_x = coord[0]
+        if coord[1] > max_y:
+            max_y = coord[1]
+        coords.append(coord)
+
+coords = sorted(coords)
+
+coords = [(i, coords[i]) for i in range(len(coords))]
 
 print(coords)
+
+stack = []
+
+grid = {}
+
+print(grid)
+while len(stack) > 0:
+
+    new_stack = []
+
+    for guy in stack:
+        guy_id, coords = guy[0], guy[1]
+
+        if grid.get(coords) is not None:
+
+
+
+    stack = new_stack
+
+
