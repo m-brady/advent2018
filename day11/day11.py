78b0becb3184c8d48c1e6c8a2dea0481c7538a33
Michael <mbrady@eyereturn.com>
2018-12-20 23:36:38 -0500

    part12

--- day11/day11.py
+++ day11/day11.py
@@ -1,6 +1,4 @@
-
 def go(serial, size, square_size):
-
     grid = [[0 for j in range(size)] for i in range(size)]
 
     for x in range(size):
@@ -18,27 +16,26 @@ def go(serial, size, square_size):
     col_sums = [sum(grid[j][i] for j in range(square_size)) for i in range(size)]
 
     square_sums = [[]]
-    cur_sum = col_sums[0] + col_sums[1]
-    for i in range(0, size - 2):
+    cur_sum = sum(col_sums[j] for j in range(square_size - 1))
+    for i in range(0, size - (square_size - 1)):
         prev_col = 0 if i == 0 else col_sums[i - 1]
-        cur_sum = cur_sum - prev_col + col_sums[i + 2]
+        cur_sum = cur_sum - prev_col + col_sums[i + (square_size - 1)]
         square_sums[0].append(cur_sum)
-
     row_sums = []
     for row in range(size):
-        row_sum, cur_sum = [], grid[row][0] + grid[row][1]
-        for col in range(size - 2):
+        row_sum, cur_sum = [], sum(grid[row][i] for i in range(square_size - 1))
+        for col in range(size - (square_size - 1)):
             prev = 0 if col == 0 else grid[row][col - 1]
-            cur_sum = cur_sum - prev + grid[row][col + 2]
+            cur_sum = cur_sum - prev + grid[row][col + (square_size - 1)]
             row_sum.append(cur_sum)
         row_sums.append(row_sum)
 
-    for row in range(1, size - 2):
+    for row in range(1, size - (square_size - 1)):
         row_squares = []
-        for col in range(0, size - 2):
+        for col in range(0, size - (square_size - 1)):
             prev_row = row_sums[row - 1][col]
             last_square = square_sums[row - 1][col]
-            square_sum = last_square - prev_row + row_sums[row + 2][col]
+            square_sum = last_square - prev_row + row_sums[row + (square_size - 1)][col]
             row_squares.append(square_sum)
         square_sums.append(row_squares)
 
@@ -47,8 +44,10 @@ def go(serial, size, square_size):
     for y in range(len(square_sums)):
         for x in range(len(square_sums[y])):
             if square_sums[y][x] > max_guy:
-                coords = (x,y)
+                coords = (x, y)
                 max_guy = square_sums[y][x]
-    print(max_guy, coords)
+    print(square_size, max_guy, coords)
 
-go(3628, 300, 3)
+
+for i in range(3628):
+    go(3628, 300, i)
